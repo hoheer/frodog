9853519175771b45b4e90de03a6ea5c2f61ac08a
siyeon lee <temperlooser@gmail.com>
2020-03-08 20:38:13 +0900

    Add files via upload

---
+++ src/vos/AUTH_FORM.java
@@ -0,0 +1,16 @@
+package vos;
+
+public class AUTH_FORM {
+	 private final String grant_type =new String( "client_credentials");
+     private final String client_id =  new String("EIWYsne8ZBe6wrgSThBlJlrXstQCZlanM3dJaqor");
+     private final String client_secret = new String
+("fICxsVZiDO1fJbbG2ga9HeZQFiPN79p9kHQfuWn0rcDE4b5jswdHo9R0W2CmcdYQD61giD9fEy3N763XZJlttpry5EDrG2AlvhgbhGEarb3e2oFyr7RixSbRRUB3Y0aT");
+
+     public String getGrant_type() {return grant_type;}
+     public String getClient_id () { return client_id; }
+     public String getClient_secret() { return client_secret; }
+        
+    
+
+
+}
